44b5e638d8257a88d9309771c159170f33f6e44c
codewith-satyam <satyam6453@gmail.com>
2026-07-07 14:54:26 +0530

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 14453
+let accountEmail = "satyam6453@gmaik.com"
+var accountPassword = "123545"
+accountCity = "jaipur"   // wrong way to declare a variable
+let accountState; // output = Undefined
+
+// accountId = 2 // not allowd
+
+console.log(accountId);
+
+
+/*
+prefer not to use var
+because  of issue in block scope and functional scope
+*/
+
+
+accountEmail="coder6453@gmail.com"
+accountPassword="85858585"
+accountCity="Gorakhpur"
+
+console.table([accountEmail, accountPassword, accountCity, accountState]);
+
+
