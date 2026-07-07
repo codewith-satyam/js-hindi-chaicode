f20a62da9818a688ca645eea73a18feab150461c
codewith-satyam <satyam6453@gmail.com>
2026-07-07 17:23:57 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,34 @@
+let score = true;
+
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33bc" => Nan
+// true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+// 1 =>.  true; 0 => false
+// "" => false
+// "satyam" => tRUE
+
+let someNumber = 6
+let stringNumber = String(someNumber)  // to convert number to string
+console.log(typeof stringNumber);
+
+
+
+
+
+
