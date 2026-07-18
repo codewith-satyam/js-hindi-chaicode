a73fdfc789fb475733e50fc2fef0bc9ef2b9e081
codewith-satyam <satyam6453@gmail.com>
2026-07-18 14:57:51 +0530

    ..

--- 01_basics/07_dates_in.js
+++ 01_basics/07_dates_in.js
@@ -1,16 +1,16 @@
 // // dates (Object)
 
 let myDate = new Date()
-// console.log(myDate.toString()); // Sat Jul 18 2026 09:11:32 GMT+0530 (India Standard Time)
-// console.log(myDate.toDateString()); // Sat Jul 18 2026
-// console.log(myDate.toLocaleString()); // 18/7/2026, 9:11:32 am
-// console.log(myDate.toISOString()); //2026-07-18T03:41:32.086Z
-// console.log(myDate.toJSON()); // 2026-07-18T03:41:32.086Z2026-07-18T03:41:32.086Z
-// console.log(myDate.toLocaleDateString()); // 18/7/2026
-// console.log(myDate.toLocaleTimeString()); // 9:11:32 am
-// console.log(myDate.toTimeString()); // 09:11:32 GMT+0530 (India Standard Time)
-// console.log(myDate.toUTCString()); //sat, 18 Jul 2026 03:41:32 GMT
-// console.log(myDate.getTimezoneOffset()); //-330
+console.log(myDate.toString()); // Sat Jul 18 2026 09:11:32 GMT+0530 (India Standard Time)
+console.log(myDate.toDateString()); // Sat Jul 18 2026
+console.log(myDate.toLocaleString()); // 18/7/2026, 9:11:32 am
+console.log(myDate.toISOString()); //2026-07-18T03:41:32.086Z
+console.log(myDate.toJSON()); // 2026-07-18T03:41:32.086Z2026-07-18T03:41:32.086Z
+console.log(myDate.toLocaleDateString()); // 18/7/2026
+console.log(myDate.toLocaleTimeString()); // 9:11:32 am
+console.log(myDate.toTimeString()); // 09:11:32 GMT+0530 (India Standard Time)
+console.log(myDate.toUTCString()); //sat, 18 Jul 2026 03:41:32 GMT
+console.log(myDate.getTimezoneOffset()); //-330
 
 // console.log(typeof myDate);  // object
 
@@ -19,9 +19,9 @@ let myCreatedDate = new Date("07-18-2026")
 
 let myTimeStamp = Date.now()
 
-// console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
 
 let newDate = new Date()
 console.log(newDate);
